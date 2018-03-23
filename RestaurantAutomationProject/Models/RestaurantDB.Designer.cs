acd8b563bd33835702b793513440cb5b6c38e786
kc_milan <macmilan100@gmail.com>
2018-03-23 12:56:58 -0500

    database changed hence deleted edmx and created new

--- RestaurantAutomationProject/Models/RestaurantDB.Designer.cs
+++ RestaurantAutomationProject/Models/RestaurantDB.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\RestaurantAutomationProject\RestaurantAutomationProject\RestaurantAutomationProject\Models\Restaurant.edmx'. 
+﻿// T4 code generation is enabled for model 'D:\680 software eng project\restaurant\RestaurantAutomationProject\Models\RestaurantDB.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
